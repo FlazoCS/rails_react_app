bcddb092b9c4bbf7dd4cc2b1a854751a11c95ce1
FlazoCS <sian_chia@mymail.sutd.edu.sg>
2024-07-04 01:09:08 +0800

    Implemented Create New Posts

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -3,13 +3,15 @@ import { Route, Routes } from "react-router-dom";
 
 import PostsList from "../features/posts/PostsList";
 import PostsDetails from "../features/posts/PostsDetails";
+import NewPostForm from "../features/posts/NewPostForm";
+
 // Add new routes here
 function AppRoutes() {
     return (
         <Routes>
             <Route path="/" element={<PostsList />} />
             <Route path="posts/:id" element={<PostsDetails />} />
-            <Route path="/new" element={<h1>New Post Form</h1>} />
+            <Route path="/new" element={<NewPostForm />} />
         </Routes>
     );
 }
